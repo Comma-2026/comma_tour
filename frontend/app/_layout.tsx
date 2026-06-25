74551688de894c17bcbf5464895335b5e05c8a7c
HJ1263532 <osisland2918@gmail.com>
2026-06-25 14:01:34 +0900

    FEAT 로그인과 로딩 프론트

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -6,7 +6,7 @@ import 'react-native-reanimated';
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
 export const unstable_settings = {
-  anchor: '(tabs)',
+  anchor: 'index',
 };
 
 export default function RootLayout() {
@@ -15,6 +15,11 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
+        {/* 스플래시(로딩) */}
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        {/* 로그인 */}
+        <Stack.Screen name="login" options={{ headerShown: false }} />
+        {/* 로그인 이후 메인 탭 */}
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
       </Stack>
