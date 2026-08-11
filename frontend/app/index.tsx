524145bd2ccbd24a8eaf0649099f6efd49b46186
Jeonsooyoung <jsooyoung05@gmail.com>
2026-08-11 21:28:16 +0900

    feat: 로고 폰트사이즈 조정

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -6,7 +6,7 @@ import { Brand } from '@/constants/theme';
 import { getToken } from '@/utils/authStorage';
 
 /** 스플래시가 떠 있는 시간(ms) */
-const SPLASH_DURATION = 1800;
+const SPLASH_DURATION = 9999998;
 
 /**
  * 앱 진입 화면. 잠깐 스플래시(쉼표 카드)를 보여준 뒤,
@@ -50,7 +50,7 @@ const styles = StyleSheet.create({
   logo: {
     fontFamily: 'NotoSerifKR_500Medium',
     color: '#FFFFFF',
-    fontSize: 42,
+    fontSize: 40,
     textAlign: 'center',
     includeFontPadding: false,
     textAlignVertical: 'center',
